453b81a9a8df73ce9b7895584c677bffb6073a70
nicolascampbell <nicodsm7@gmail.com>
2022-12-01 19:53:41 +0100

    feat: changed to gif-> video with mouse-acceleration. Refactored code a bit

--- components/CustomNavbar.jsx
+++ components/CustomNavbar.jsx
@@ -1,46 +1,37 @@
 import React from "react";
 import { Nav, Navbar, Container, Col, Row } from "react-bootstrap";
-import RotatingText from "./RotatingText";
+import RotatingText from "./RotatingTextNavbar";
 import { useRouter } from "next/router";
 
 export const CustomNavbar = () => {
   const router = useRouter();
   const getLink = (path) => `${router.basePath}${path}`;
 
-  console.log(router.pathname.slice(1));
   return (
     <Navbar bg="light" expand="lg" className="custom-navbar m-lg-3">
-      <Container fluid>
-        <Row className="w-100 p-0 m-0">
-          <Col lg="2" className="d-flex align-items-center">
-            <Navbar.Brand href={getLink("/")}>
-              <RotatingText />
-            </Navbar.Brand>
-          </Col>
-          <Col lg={{ span: 2, offset: 8 }}>
-            <Nav className="me-auto flex-column">
-              <Nav.Link
-                href={getLink("/about")}
-                disabled={router.pathname.slice(1) === "about"}
-              >
-                About
-              </Nav.Link>
-              <Nav.Link
-                href={getLink("/photos")}
-                disabled={router.pathname.slice(1) === "photos"}
-              >
-                Photos
-              </Nav.Link>
-              <Nav.Link
-                href={getLink("/misc")}
-                disabled={router.pathname.slice(1) === "misc"}
-              >
-                Miscellaneous
-              </Nav.Link>
-            </Nav>
-          </Col>
-        </Row>
-      </Container>
+      <Navbar.Brand href={getLink("/")} className="m-0">
+        <RotatingText />
+      </Navbar.Brand>
+      <Nav className="flex-column">
+        <Nav.Link
+          href={getLink("/about")}
+          disabled={router.pathname.slice(1) === "about"}
+        >
+          About
+        </Nav.Link>
+        <Nav.Link
+          href={getLink("/photos")}
+          disabled={router.pathname.slice(1) === "photos"}
+        >
+          Photos
+        </Nav.Link>
+        <Nav.Link
+          href={getLink("/misc")}
+          disabled={router.pathname.slice(1) === "misc"}
+        >
+          Miscellaneous
+        </Nav.Link>
+      </Nav>
     </Navbar>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
-import React from "react";
-import Image from "next/future/image";
+import React, { useEffect, useRef, useState } from "react";
 import { useRouter } from 'next/router';
 import { Container, Col, Row } from "react-bootstrap";
 import { CustomNavbar } from "../components/CustomNavbar";
@@ -7,20 +6,67 @@ import { Footer } from "../components/Footer";
 export default function Home() {
 	const router = useRouter();
 	const getLink = (path) => `${router.basePath}${path}`;
+	const body = useRef(null)
+	const videoRef = useRef();
+	const [playbackRate, setPlaybackRate] = useState(0.3);
+	let timestamp = Date.now();
+	let lastMouseX = null;
+	let lastMouseY = null;
+
+	useEffect(() => {
+		if (body)
+			body.current.addEventListener("mousemove", function (e) {
+				if (timestamp === null) {
+					timestamp = Date.now();
+					lastMouseX = e.screenX;
+					lastMouseY = e.screenY;
+					return;
+				}
+				let now = Date.now();
+				let dt = now - timestamp;
+
+				if (dt > 100) {
+					let dx = e.screenX - lastMouseX;
+					let dy = e.screenY - lastMouseY;
+					const playbackSpeed = Math.abs(Math.round(dx / dt * 100)) + Math.abs(Math.round(dy / dt * 100))
+					if (playbackSpeed < 50) {
+						setPlaybackRate(0.3)
+					} else if (playbackSpeed < 100) {
+						setPlaybackRate(0.6)
+					} else if (playbackSpeed < 500) {
+						setPlaybackRate(1.2)
+					} else {
+						setPlaybackRate(1.5)
+					}
+					timestamp = now;
+					lastMouseX = e.screenX;
+					lastMouseY = e.screenY;
+				}
+			});
+		return () => {
+			if (body)
+				body.current?.removeEventListener('mousemove', () => console.log('abort'));
+		};
+	}, [body]);
+	useEffect(() => {
+		videoRef.current.playbackRate = playbackRate;
+		const timer = setTimeout(() => {
+			if (videoRef.current.playbackRate > 0.3)
+				setPlaybackRate(0.3)
+		}, 5000);
+		return () => clearTimeout(timer);
+	}, [playbackRate]);
+
 	return (
 		<React.Fragment>
-			<Container style={{ height: "100vh" }} fluid>
+			<Container style={{ height: "100vh" }} fluid ref={body}>
 				<CustomNavbar />
 				<Row>
 					<Col lg={{ span: 6, offset: 3 }} className="d-flex justify-content-center">
-						<Image
-							className="home-gif"
-							src={getLink("/home-gif.webp")}
-							layout="fill"
-							width="600"
-							height="600"
-							alt="Gif with some photos of Matias"
-						></Image>
+						<video ref={videoRef} autoPlay loop className="home-images" width="600"
+							height="600">
+							<source src={getLink("/home-vid.mp4")} type="video/mp4" />
+						</video>
 					</Col>
 				</Row>
 			</Container>
