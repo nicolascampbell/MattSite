ee56e592df23ede5f783dcd60d92df45d70c4a36
nicolascampbell <nicodsm7@gmail.com>
2022-12-13 19:42:06 +0100

    fix: disabled eslint

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	images: {
-		unoptimized: true,
-	},
-	basePath: '/MattSite',
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/MattSite",
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
