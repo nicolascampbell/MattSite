f722356f2ec6386ad2bbe0542d1ba6260cdc4d8f
nicolascampbell <nicodsm7@gmail.com>
2023-01-26 09:59:23 +0100

    wip: about page is connected with strapi in localhost

--- pages/about.jsx
+++ pages/about.jsx
@@ -3,53 +3,39 @@ import { Container, Col, Row } from 'react-bootstrap'
 import { useRouter } from 'next/router'
 import InstagramIcon from '@mui/icons-material/Instagram'
 import AlternateEmailIcon from '@mui/icons-material/AlternateEmail'
+export const getStaticProps = async () => {
+  const resulting = await fetch(
+    `http://localhost:1337/api/about-page?populate=*`
+  )
+  const result = await resulting.json()
 
-const about = () => {
+  return {
+    props: {
+      title: result.data.attributes.title,
+      description: result.data.attributes.description,
+      image: result.data.attributes.image
+    }
+  }
+}
+const defaultTitle = 'Desgreñado'
+const defaultDescription =
+  'Bienvenue a la pagina de Matias. Si bien Matias y su cabeza no entran en esta pagina hacemos lo que podemos Aca podes llegar a encontrar cosas de muy distinta indole un poco a la par de como se siente interactuar con el. Te distraes y esta haciendo alfombras. Si queres experimentar mas sobre esta bella persona podes encontrarlo en los links de abajo.'
+const about = ({
+  title = defaultTitle,
+  description = defaultDescription,
+  image
+}) => {
   const router = useRouter()
   const getLink = (path) => `${router.basePath}${path}`
-
-  // const body = useRef(null)
-  // const specialCol = useRef()
-  // let timestamp = Date.now()
-  // let lastMouseX = null
-  // let lastMouseY = null
-
-  // useEffect(() => {
-  //   if (body)
-  //     body.current.addEventListener('mousemove', function (e) {
-  //       if (timestamp === null) {
-  //         timestamp = Date.now()
-  //         lastMouseX = e.screenX
-  //         lastMouseY = e.screenY
-  //         return
-  //       }
-  //       let now = Date.now()
-  //       let dt = now - timestamp
-  //       const dx = Math.abs(e.screenX - lastMouseX)
-  //       const dy = Math.abs(e.screenY - lastMouseY)
-  //       if (dt > 300 && specialCol.current && dx + dy > 200) {
-  //         specialCol.current.scrollTo(e.screenX * 2, e.screenY * 2)
-  //         timestamp = now
-  //         lastMouseX = e.screenX
-  //         lastMouseY = e.screenY
-  //       }
-  //     })
-  //   return () => {
-  //     if (body)
-  //       body.current?.removeEventListener('mousemove', () =>
-  //         console.log('abort')
-  //       )
-  //   }
-  // }, [body])
 
   return (
     <Container fluid className="about-page p-4  ">
       <Row>
-        <Col xs={12}  md={{ span: 4, offset: 1 }}>
+        <Col xs={12} md={{ span: 4, offset: 1 }}>
           <img
-            src={getLink('/about_mati.webp')}
-            srcSet={getLink('/about_mati.webp')}
-            alt={'Foto de Matias'}
+            src={'http://localhost:1337' + image.data.attributes.url}
+            srcSet={'http://localhost:1337' + image.data.attributes.url}
+            alt={image.data.attributes.alternativeText}
             loading="lazy"
             height="auto"
             width={'100%'}
@@ -57,17 +43,8 @@ const about = () => {
         </Col>
         <Col xs={12} md={{ span: 7 }}>
           <div>
-            <h5>Desgreñado</h5>
-            <p style={{ whiteSpace: 'pre-line' }}>
-              Bienvenue a la pagina de Matias. Si bien Matias y su cabeza no
-              entran en esta pagina hacemos lo que podemos.
-            </p>
-            <p style={{ whiteSpace: 'pre-line' }}>
-              Aca podes llegar a encontrar cosas de muy distinta indole un poco
-              a la par de como se siente interactuar con el. Te distraes y esta
-              haciendo alfombras. Si queres experimentar mas sobre esta bella
-              persona podes encontrarlo en los links de abajo.
-            </p>
+            <h5>{title}</h5>
+            <p style={{ whiteSpace: 'pre-line' }}>{description}</p>
           </div>
           <div>
             <a
